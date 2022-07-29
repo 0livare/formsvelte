1b5b8965ec85f05d42666c8d262a75fde8e8937c
Zach Posten <zach@posten.io>
2022-07-29 14:26:00 -0500

    chore: Add named exports

---
+++ src/lib/index.ts
@@ -0,0 +1,6 @@
+import Formsvelte from './formsvelte.svelte'
+import Form from './form.svelte'
+import Field from './field.svelte'
+import Error from './error.svelte'
+
+export { Formsvelte, Form, Field, Error }
